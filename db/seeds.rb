6c24faab425093f90ae581d5bb58fd9dc07116aa
fr3d100 <f.bonnand@gmail.com>
2019-01-30 17:01:56 +0100

    Models fonctionnels

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Utilisation de la gem faker
+require 'faker'
+
+# On supprime les données en base
+User.destroy_all
+Pin.destroy_all
+Comment.destroy_all
+
+# On insère 5 users
+5.times do
+	User.create(username: Faker::Internet.username(8), email: Faker::Internet.email)
+end
+puts "5 users ont été insérés"
+
+# On créé 10 pins qu'on associé aléatoirement à des users
+10.times do 
+	Pin.create(url: Faker::Internet.url, user: User.order("RANDOM()").first)
+end	
+puts "10 pins ont été insérés"
+
+# On créé 50 commentaires qu'on associé aléatoirement à des users et à des pins
+50.times do 
+	Comment.create(content: Faker::HarryPotter.quote, pin: Pin.order("RANDOM()").first , user: User.order("RANDOM()").first )
+end	
+puts "50 commentaires ont été insérés"
+
+
